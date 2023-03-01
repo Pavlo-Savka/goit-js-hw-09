dfc8d327aabc2d23bd53f5ec7bd45e48e15cd96b
Pavlo-Savka <savka.pavlo@gmail.com>
2023-03-01 22:56:37 +0200

    Prettier

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,26 +1,23 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-};
+}
 const body = document.querySelector('body');
 const buttonStart = document.querySelector('button[data-start]');
 const buttonStop = document.querySelector('button[data-stop]');
 let intervalID;
 buttonStop.setAttribute('disabled', 'disabled');
 
-const start = document.querySelector('button[data-start]')
-    .addEventListener('click', () => {
-        buttonStart.setAttribute('disabled', 'disabled');
-        buttonStop.removeAttribute('disabled');
-        body.style.backgroundColor = getRandomHexColor();
-        intervalID = setInterval(() => {
-            body.style.backgroundColor = getRandomHexColor();
-        }, 1000);
-    });
+const start = buttonStart.addEventListener('click', () => {
+  buttonStart.setAttribute('disabled', 'disabled');
+  buttonStop.removeAttribute('disabled');
+  body.style.backgroundColor = getRandomHexColor();
+  intervalID = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
 
-  const stop = document.querySelector('button[data-stop]')
-      .addEventListener('click', () => {
-          buttonStart.removeAttribute('disabled');
-          buttonStop.setAttribute('disabled', 'disabled');
-          clearInterval(intervalID);
-      }); 
-
+const stop = buttonStop.addEventListener('click', () => {
+  buttonStart.removeAttribute('disabled');
+  buttonStop.setAttribute('disabled', 'disabled');
+  clearInterval(intervalID);
+});
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,32 +1,31 @@
 import Notiflix from 'notiflix';
 const form = document.querySelector('.form');
 const createPromise = (position, step) => {
-const promise = new Promise((resolve, reject) => {
+  const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
-        const shouldResolve = Math.random() > 0.3;
-        if (shouldResolve) {
-          resolve(`Fulfilled promise  ${position} in ${step}ms`);
-        } else {
-          reject(`Rejected promise ${position} in ${step}ms`);
-        }
-      }, 
-      step);
-    })
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve(`Fulfilled promise  ${position} in ${step}ms`);
+      } else {
+        reject(`Rejected promise ${position} in ${step}ms`);
+      }
+    }, step);
+  });
   return promise;
-  };
-form.addEventListener("submit", handleSubmit);
+};
+form.addEventListener('submit', handleSubmit);
 function handleSubmit(event) {
   event.preventDefault();
- const delay = parseInt(form.delay.value);
- const step = (form.step.value);
- const amount = form.amount.value;
+  const delay = parseInt(form.delay.value);
+  const step = form.step.value;
+  const amount = form.amount.value;
   for (let i = 0; i < amount; i += 1) {
-   createPromise(i + 1, step * i + delay)
-    .then(value => {
-      Notiflix.Notify.success(value);
+    createPromise(i + 1, step * i + delay)
+      .then(value => {
+        Notiflix.Notify.success(value);
       })
-    .catch(error => { 
-      Notiflix.Notify.failure(error);
-    })
+      .catch(error => {
+        Notiflix.Notify.failure(error);
+      });
   }
-};
+}
