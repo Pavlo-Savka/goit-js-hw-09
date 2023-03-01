e33a21f1f4c90997a46c00087595ea28657508f9
Pavlo-Savka <savka.pavlo@gmail.com>
2023-03-01 23:13:49 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -7,7 +7,7 @@ const buttonStop = document.querySelector('button[data-stop]');
 let intervalID;
 buttonStop.setAttribute('disabled', 'disabled');
 
-const start = buttonStart.addEventListener('click', () => {
+buttonStart.addEventListener('click', () => {
   buttonStart.setAttribute('disabled', 'disabled');
   buttonStop.removeAttribute('disabled');
   body.style.backgroundColor = getRandomHexColor();
@@ -16,7 +16,7 @@ const start = buttonStart.addEventListener('click', () => {
   }, 1000);
 });
 
-const stop = buttonStop.addEventListener('click', () => {
+buttonStop.addEventListener('click', () => {
   buttonStart.removeAttribute('disabled');
   buttonStop.setAttribute('disabled', 'disabled');
   clearInterval(intervalID);
